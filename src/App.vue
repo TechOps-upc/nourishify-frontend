e44d4eff9d1c88377b1d5407b6446c75029cf480
LordUB <86518129+LordUB@users.noreply.github.com>
2023-09-25 12:23:33 -0500

    feat(header-content):Header content images added, header component added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-
+  <div>
+    <header-content></header-content>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script >
-
-
+  import HeaderContent from '@/shared/components/header-content.component.vue'
+  export default{
+    name:"App",
+    components:{HeaderContent},
+  }
 
 </script>
 
 <style >
-
+  body, html, div, header {
+    margin: 0;
+    padding: 0;
+  }
 </style>
