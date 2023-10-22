37ecf9ed532d0ddab1acfcfe5f82a89fb67a6d11
Adrian Melgar <DaikiMT@outlook.es>
2023-10-21 19:11:27 -0500

    Added plan selection view

--- src/subscription-plans/pages/subscription-plan.component.vue
+++ src/subscription-plans/pages/subscription-plan.component.vue
@@ -1,17 +1,21 @@
 <template>
-  <h1 id="title">Select a Plan</h1>
-  <div class="plan-card">
-    <div v-for="plan in subscriptionPlans" :key="plan.id">
-      <pv-card class="content">
-        <template #header>
-          <h3>{{ plan.name }}</h3>
-          <h2>{{plan.price}}</h2>
-          <pv-divider></pv-divider>
-        </template>
-        <template #content>
-          <p v-for="perk in plan.perks" :key="perk.id"><i class="pi pi-check-square"></i> {{ perk }}</p>
-        </template>
-      </pv-card>
+  <div class="a">
+    <h1 id="title">Select a Plan</h1>
+    <div class="plan-card">
+      <div v-for="plan in subscriptionPlans" :key="plan.id">
+        <pv-card class="content">
+          <template #header>
+            <h3>{{ plan.name }}</h3>
+            <h2>{{ formattedPrice(plan.price) }}</h2>
+            <pv-divider></pv-divider>
+          </template>
+          <template #content>
+            <p v-for="perk in plan.perks" :key="perk.id" class="m-0 p-0"><i class="pi pi-check-square"></i> {{ perk }}
+            </p>
+            <pv-button label="Comenzar prueba" @click="" class="btn" rounded></pv-button>
+          </template>
+        </pv-card>
+      </div>
     </div>
   </div>
 </template>
@@ -32,6 +36,14 @@ export default {
     this.subscriptionPlansService.getAll().then((response) => {
       this.subscriptionPlans = response.data
     })
+  },
+  computed: {
+    formattedPrice() {
+      return (price) => new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(price);
+    },
   }
 }
 
@@ -43,8 +55,9 @@ export default {
   justify-content: space-between;
 }
 
-#title  {
-  margin: 2rem;
+#title {
+  margin: 0 2rem;
+  padding: 2rem 0;
 }
 
 .content {
@@ -52,4 +65,17 @@ export default {
   margin: 0 2rem;
 }
 
+.a {
+  height: 100vh;
+
+  background-color: #D1EEB4;
+}
+
+.btn {
+  background-color: #67E0A3;
+  color: #fff;
+  margin-top: 2rem;
+  font-weight: bold;
+
+}
 </style>
