a2f494fd0afbe2cec4af04209e6e3be7cc4565de
Sebastian Bustinza <sebabumu@hotmail.com>
2023-10-22 21:22:20 -0500

    feat(register):Implemented register  view

---
+++ src/register-form/pages/register-form-page.component.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div class="image-container">
+      <img src="/register-form-images/register-form.png" alt="Description of the Image" class="register-image">
+    </div>
+    <div class="form-container">
+      <registerFormComponent user-role="client"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import registerFormComponent from "@/register-form/components/register-form.component.vue";
+export default {
+  components: {
+    registerFormComponent
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  height: 70vh;
+  margin-top: 5%;
+}
+
+.image-container, .form-container {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.register-image{
+  width: 65%;
+}
+
+
+</style>
