3e4d4367657f09f903d3c1b33c82e1d61a31e07a
CholoHack <116128035+CholoHack@users.noreply.github.com>
2023-10-24 01:16:04 -0500

    feat: implemented work-out-plans
    implemented work out plan component

---
+++ src/work-out-plan/pages/work-out-plan.component.vue
@@ -0,0 +1,22 @@
+<template>
+  <toolbar-component></toolbar-component>
+  <work-out-plan></work-out-plan>
+</template>
+
+<script>
+import toolbarComponent from "@/shared/components/client-toolbar.component.vue";
+import profileDataComponent from "@/profile/components/clients-profile-data.component.vue";
+import workOutPlan from '@/work-out-plan/components/work-out-plan-data.component.vue'
+
+export default {
+  name: "SimpleWorkOutTable",
+  components:{
+    workOutPlan,
+    toolbarComponent
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
