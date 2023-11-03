4b8368fb7593ac52b4d56ad31e87b36880a7babd
CholoHack <116128035+CholoHack@users.noreply.github.com>
2023-11-03 03:02:52 -0500

    core: i18n functioning on app.vue

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -3,8 +3,11 @@ import messages from "@intlify/unplugin-vue-i18n/messages";
 
 export const i18n = createI18n({
     legacy: false,
-    locales: 'en',
-    fallbackLocale: 'es',
-    availableLocales: ['es','en'],
+    globalInjection: true,
+    locale: "es",
+    //para idioma del navegador
+    //locale: window.navigator.language.substring(0,2) | "en",
+    fallbackLocale: "es",
+    availableLocales: ["es","en"],
     messages: messages,
 })
